f6c94c7618e598a64132300a242394d44a9d05aa
ShaikhRohan <106794160+ShaikhRohan@users.noreply.github.com>
2022-08-17 01:16:42 +0500

    Video 49 Hashing Passwords using bcryptjs in NodeJs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 //create a user usig Post "/api/auth/createuser" does not require authentication
-
+const JWT_SECRET = 'shaikhrohan@3d'
 router.post(
   "/createuser",
   [
@@ -29,18 +31,33 @@ router.post(
       if (user) {
         return res.status(400).json({ error: "This email is already taken" });
       }
-
+      //create salt using bcryptjs
+      const salt = await bcrypt.genSalt(10)
+      //create new user
+      //secPassword = req.body .password
+      // The function which return promise and write in the async function we use await keyword
+      const secPassword = await bcrypt.hash(req.body.password,salt);
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        //password: req.body.password,
+        password: secPassword
       });
 
       //   .then(user => res.json(user)).catch(err=> {console.log(err)
       // res.json({error: 'This email is already taken ', message: err.message})} )
       //res.send(req.body)
       //res.json({'Nice': 'nice'})
-      res.json(user);
+      //we did not send res.json we sign the token 
+      const data = {
+        user:{
+            id:user.id
+        }
+      }
+      const authToken = jwt.sign(data, JWT_SECRET);
+      //console.log(jwtData);
+      //res.json(user);
+      res.json({authToken});
     } catch (error) {
       console.error(error.message);
       res.status(500).send("some error occured");
@@ -49,3 +66,5 @@ router.post(
 );
 
 module.exports = router;
+//install jason web token
+//install bcryptjs
