925637396928b73887f5263dc791cce1ba1c0012
ShaikhRohan <106794160+ShaikhRohan@users.noreply.github.com>
2022-08-16 22:24:32 +0500

    Video 45 Storing Data into the Database Using Mongoose Model

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 connectToMongo();
 const app = express()
 const port = 3000
+//use middleware
+app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth.js'))
 app.use('/api/notes', require('./routes/notes.js'))
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
+//create a user usig Post "/api/auth" does not require authentication
 
-router.get('/', (req,res)=>{
+router.post('/', (req,res)=>{
     // obj ={
     //     name :"Rohan",
     //     number: 24
     // }
-    res.json([])
+    const user = User(req.body)
+    user.save();
+    console.log(req.body);
+    res.send(req.body)
 })
 
 module.exports = router
