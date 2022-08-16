b6acd95da2a94ab274e110adf8442fb6cecb2215
ShaikhRohan <106794160+ShaikhRohan@users.noreply.github.com>
2022-08-17 00:04:50 +0500

    Video 47 Creating ThunderClient Collections to Manage Requests

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,28 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-   name:{
-    type: String,
-    required: true
-   },
-   email:{
+  name: {
     type: String,
     required: true,
-    unique: true
-   },
-   password:{
+  },
+  email: {
     type: String,
-    required: true
-   },
-   date:{
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
     type: Date,
-    default: Date.now
-   }
-  });
+    default: Date.now,
+  },
+});
 
-  //module.exports = mongoose.model('user',UserSchema);
-  const User = mongoose.model('user',UserSchema)
-  User.createIndexes();
-  module.exports = User
-  //we use schema in the routes folder
+//module.exports = mongoose.model('user',UserSchema);
+const User = mongoose.model("user", UserSchema);
+//User.createIndexes();
+module.exports = User;
+//we use schema in the routes folder
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User')
-const { body, validationResult } = require('express-validator');
-//create a user usig Post "/api/auth" does not require authentication
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
+//create a user usig Post "/api/auth/createuser" does not require authentication
 
-router.post('/',[ 
-    body('name').isLength({ min: 5 }),
-    body('email').isEmail(),
-    body('password').isLength({ min: 5 })], (req,res)=>{
+router.post(
+  "/createuser",
+  [
+    body("name").isLength({ min: 5 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
     // obj ={
     //     name :"Rohan",
     //     number: 24
@@ -19,14 +23,29 @@ router.post('/',[
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    //check whether email is exist already it is promise so use await
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "This email is already taken" });
+      }
 
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      }).then(user => res.json(user)).catch(err=> {console.log(err)
-    res.json({error: 'This email is already taken ', message: err.message})} )
-     //res.send(req.body)
-})
+      });
 
-module.exports = router
+      //   .then(user => res.json(user)).catch(err=> {console.log(err)
+      // res.json({error: 'This email is already taken ', message: err.message})} )
+      //res.send(req.body)
+      //res.json({'Nice': 'nice'})
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occured");
+    }
+  }
+);
+
+module.exports = router;
