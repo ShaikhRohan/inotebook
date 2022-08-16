17afded8af6fc31fd98b68c6f5c0a59db1022446
ShaikhRohan <106794160+ShaikhRohan@users.noreply.github.com>
2022-08-16 23:08:57 +0500

    Video 46 Adding Data Validation Using express-validator

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,5 +21,8 @@ const UserSchema = new Schema({
    }
   });
 
-  module.exports = mongoose.model('user',UserSchema);
+  //module.exports = mongoose.model('user',UserSchema);
+  const User = mongoose.model('user',UserSchema)
+  User.createIndexes();
+  module.exports = User
   //we use schema in the routes folder
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 //create a user usig Post "/api/auth" does not require authentication
 
-router.post('/', (req,res)=>{
+router.post('/',[ 
+    body('name').isLength({ min: 5 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 })], (req,res)=>{
     // obj ={
     //     name :"Rohan",
     //     number: 24
     // }
-    const user = User(req.body)
-    user.save();
-    console.log(req.body);
-    res.send(req.body)
+    // const user = User(req.body)
+    // user.save();
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user)).catch(err=> {console.log(err)
+    res.json({error: 'This email is already taken ', message: err.message})} )
+     //res.send(req.body)
 })
 
 module.exports = router
