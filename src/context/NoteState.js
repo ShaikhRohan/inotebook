d7cba9ab72d803935ce9734575d4c40a9b2914ea
ShaikhRohan <106794160+ShaikhRohan@users.noreply.github.com>
2022-08-20 05:23:43 +0500

    Video 64 Adding "delete note" functionality to iNotebook

--- src/context/NoteState.js
+++ src/context/NoteState.js
@@ -28,7 +28,18 @@ const NoteState = (props) => {
         "date": "2022-08-17T17:43:40.136Z",
         "__v": 0
       
-    }
+    },
+    {
+      
+      "_id": "ccd47903dcb943f9a3",
+      "user": "62fd071c4c7c78c998233b5b",
+      "title": "Imagiion",
+      "description": "My imagination is that i will be the top skilled person and teach the skills to others",
+      "tag": "1st Note",
+      "date": "2022-08-17T17:43:40.136Z",
+      "__v": 0
+    
+  }
     
   
   ]
@@ -50,7 +61,8 @@ const NoteState = (props) => {
     }
     //delete a note 
     const deleteNote =(id)=>{
-
+      const newNotes = notes.filter((note)=>{return note._id!==id})
+      setNotes(newNotes)
     }
     //update a note 
     const updateNote = (id)=>{
